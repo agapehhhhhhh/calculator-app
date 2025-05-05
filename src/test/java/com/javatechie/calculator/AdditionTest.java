2fa298f719b0af2eeae3d770bdda659c4d339fdf
frzxyz <75243346+frzxyz@users.noreply.github.com>
2025-05-05 23:19:32 +0700

    Update AdditionTest.java

--- src/test/java/com/javatechie/calculator/AdditionTest.java
+++ src/test/java/com/javatechie/calculator/AdditionTest.java
@@ -6,16 +6,30 @@ import static org.junit.Assert.*;
 public class AdditionTest {
 
     @Test
-    public void addition_shouldReturnCorrectResult() {
-        // (1) setup
+    public void testPositiveAddition() {
         Addition op = new Addition();
-        int a = 10, b = 5;
-        int expected = 15;
+        int actual = op.calculate(250, 254);
+        assertEquals("250 + 254 should equal 504", 504, actual);
+    }
 
-        // (2) exercise
-        int actual = op.calculate(a, b);
+    @Test
+    public void testNegativeAddition() {
+        Addition op = new Addition();
+        int actual = op.calculate(-125, -323);
+        assertEquals("-125 + (-323) should equal -448", -448, actual);
+    }
 
-        // (3) verify
-        assertEquals("10 + 5 should equal 15", expected, actual);
+    @Test
+    public void testAdditionWithZero() {
+        Addition op = new Addition();
+        int actual = op.calculate(223, 0);
+        assertEquals("223 + 0 should equal 223", 223, actual);
+    }
+
+    @Test
+    public void testPositiveAndNegativeAddition() {
+        Addition op = new Addition();
+        int actual = op.calculate(122, -133);
+        assertEquals("122 + (-133) should equal -11", -11, actual);
     }
 }
