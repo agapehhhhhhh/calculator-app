862b8258a8c005c28f035a4d7e136287feee7582
frzxyz <75243346+frzxyz@users.noreply.github.com>
2025-05-05 23:19:48 +0700

    Update DivisionTest.java

--- src/test/java/com/javatechie/calculator/DivisionTest.java
+++ src/test/java/com/javatechie/calculator/DivisionTest.java
@@ -6,19 +6,36 @@ import static org.junit.Assert.*;
 public class DivisionTest {
 
     @Test
-    public void division_shouldReturnCorrectResult() {
+    public void testPositiveDivision() {
         Division op = new Division();
-        int a = 20, b = 5;
-        int expected = 4;
+        int actual = op.calculate(60, 30);
+        assertEquals("60 / 30 should equal 2", 2, actual);
+    }
 
-        int actual = op.calculate(a, b);
+    @Test
+    public void testNegativeDivision() {
+        Division op = new Division();
+        int actual = op.calculate(-125, -25);
+        assertEquals("-125 / -25 should equal 5", 5, actual);
+    }
 
-        assertEquals("20 / 5 should equal 4", expected, actual);
+    @Test
+    public void testZeroNumeratorDivision() {
+        Division op = new Division();
+        int actual = op.calculate(0, 223);
+        assertEquals("0 / 223 should equal 0", 0, actual);
+    }
+
+    @Test
+    public void testPositiveAndNegativeDivision() {
+        Division op = new Division();
+        int actual = op.calculate(100, -100);
+        assertEquals("100 / -100 should equal -1", -1, actual);
     }
 
     @Test(expected = ArithmeticException.class)
-    public void divisionByZero_shouldThrowArithmeticException() {
+    public void testDivisionByZero_shouldThrowException() {
         Division op = new Division();
-        op.calculate(10, 0); // Expected to throw ArithmeticException
+        op.calculate(10, 0); // Will throw ArithmeticException
     }
 }
