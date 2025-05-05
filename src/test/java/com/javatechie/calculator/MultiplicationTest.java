f2cca021fa2d6b39361382c1be63e1f9bd6e87b7
frzxyz <75243346+frzxyz@users.noreply.github.com>
2025-05-05 23:20:01 +0700

    Update MultiplicationTest.java

--- src/test/java/com/javatechie/calculator/MultiplicationTest.java
+++ src/test/java/com/javatechie/calculator/MultiplicationTest.java
@@ -6,13 +6,30 @@ import static org.junit.Assert.*;
 public class MultiplicationTest {
 
     @Test
-    public void multiplication_shouldReturnCorrectResult() {
+    public void testPositiveMultiplication() {
         Multiplication op = new Multiplication();
-        int a = 4, b = 5;
-        int expected = 20;
+        int actual = op.calculate(250, 254);
+        assertEquals("250 * 254 should equal 63500", 63500, actual);
+    }
 
-        int actual = op.calculate(a, b);
+    @Test
+    public void testNegativeMultiplication() {
+        Multiplication op = new Multiplication();
+        int actual = op.calculate(-125, -323);
+        assertEquals("-125 * -323 should equal 40375", 40375, actual);
+    }
 
-        assertEquals("4 * 5 should equal 20", expected, actual);
+    @Test
+    public void testMultiplicationWithZero() {
+        Multiplication op = new Multiplication();
+        int actual = op.calculate(223, 0);
+        assertEquals("223 * 0 should equal 0", 0, actual);
+    }
+
+    @Test
+    public void testPositiveAndNegativeMultiplication() {
+        Multiplication op = new Multiplication();
+        int actual = op.calculate(122, -133);
+        assertEquals("122 * -133 should equal -16226", -16226, actual);
     }
 }
